82f5279b0f1c5b1937498f5dcfafa9366b656f86
austin <austin.chase.m@gmail.com>
2019-11-19 10:05:54 +0800

    basic python tester

--- scripts/determine_threshold.py
+++ scripts/determine_threshold.py
@@ -28,24 +28,7 @@ def construct_cmd(is_release, data_dir, window_length, alert_threshold):
 
     return cmd
 
-@click.command()
-def main():
-    adp_regex = r'^.*ADP stats:, std_dev: (.*), avg: (.*), max: (.*), min: (.*)$'
-    adp_regex = re.compile(adp_regex)
-    pos_regex = r'^.*num_stars: (.*), num_false_events: (.*), num_true_events: (.*), num_events.*$'
-    pos_regex = re.compile(pos_regex)
-
-    is_release = True
-    data_dir = "/home/austin/research/microlensing_star_data/star_subset"
-    window_length = 30
-    alert_threshold = 0.1
-    proc = subprocess.run(
-        construct_cmd(is_release, data_dir, window_length, alert_threshold),
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, encoding='utf8'
-    )
-
-    output = proc.stdout
-
+def parse_results(output):
     # NOTE: remove ansi text coloring
     # from https://stackoverflow.com/a/14693789
     # 7-bit C1 ANSI sequences
@@ -58,10 +41,83 @@ def main():
     ''', re.VERBOSE)
     output = ansi_escape.sub('', output)
 
+    adp_regex = r'^.*ADP stats:, std_dev: (.*), avg: (.*), max: (.*), min: (.*)$'
+    adp_regex = re.compile(adp_regex)
+    pos_regex = r'^.*num_stars: (.*), num_false_events: (.*), num_true_events: (.*), num_events.*$'
+    pos_regex = re.compile(pos_regex)
+
+    adp = {}
+    pos = {}
     for line in output.splitlines():
-        print(adp_regex.findall(line))
-        print(pos_regex.findall(line))
-    print(output)
+        adps = adp_regex.findall(line)
+        poss = pos_regex.findall(line)
+
+        if len(adps) != 0:
+            adps = adps[0]
+            adp = {
+                'std_dev': float(adps[0]),
+                'avg': float(adps[1]),
+                'max': float(adps[2]),
+                'min': float(adps[3]),
+            }
+        elif len(poss) != 0:
+            poss = poss[0]
+            pos = {
+                'num_stars': int(poss[0]),
+                'num_false_events': int(poss[1]),
+                'num_true_events': int(poss[2]),
+                'num_events': int(poss[1]) + int(poss[2]),
+            }
+    return adp, pos
+
+@click.command()
+def main():
+    is_release = True
+    data_dir = "/home/austin/research/microlensing_star_data/star_subset"
+    window_length = 30
+    alert_threshold_window = [0.0, 200.0]
+    alert_threshold = 0.0
+    alert_threshold_prev = 100.0
+
+    pos = None
+    adp = None
+    # converge on optimal value
+    while abs(alert_threshold - alert_threshold_prev) > 0.001:
+        # do a binary search like search for finding optimal value
+        # -- not useful for simple case -- but good for when we integrate reject testing
+        if pos and pos['num_false_events'] > 0:
+            alert_threshold_window = (alert_threshold, alert_threshold_window[1])
+        elif pos:
+            alert_threshold_window = (alert_threshold_window[0], alert_threshold)
+
+
+        alert_threshold_prev = alert_threshold
+        alert_threshold = (alert_threshold_window[0] + alert_threshold_window[1])/2.0
+
+        proc = subprocess.run(
+            construct_cmd(is_release, data_dir, window_length, alert_threshold),
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, encoding='utf8'
+        )
+        output = proc.stdout
+        adp, pos = parse_results(output)
+
+        print("Alert Threshold: {}".format(alert_threshold))
+        print("ADP Stats: {}".format(adp))
+        print("Positives Stats: {}".format(pos))
+
+    while pos['num_false_events'] > 0:
+        alert_threshold += 0.0001
+
+        proc = subprocess.run(
+            construct_cmd(is_release, data_dir, window_length, alert_threshold),
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, encoding='utf8'
+        )
+        output = proc.stdout
+        adp, pos = parse_results(output)
+
+        print("Alert Threshold: {}".format(alert_threshold))
+        print("ADP Stats: {}".format(adp))
+        print("Positives Stats: {}".format(pos))
 
 if __name__ == '__main__':
     main()
